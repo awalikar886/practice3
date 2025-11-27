98a7cd7280c06e1901a7d6cb293d6cd961cd5ca9
awalikar886 <awalikar886@gmail.com>
2025-11-27 15:18:59 +0530

    Add temperature alert script

---
+++ temperature.py
@@ -0,0 +1,14 @@
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python temperature_alert.py <temperature>")
+    sys.exit()
+
+temp = float(sys.argv[1])
+
+if temp < 15:
+    print("Cold")
+elif temp <= 30:
+    print("Normal")
+else:
+    print("Hot")
